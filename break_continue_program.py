562afcd45ba0732313de4641b490962f6cdc6bde
Shaunak Chitare <shaunak.chitare@gmail.com>
2020-07-06 20:45:58 +0530

    break and continue statement practice

---
+++ break_continue_program.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+
+# Break
+for letter in "shaunak":
+    if letter == "a":
+        break
+    print(letter)
+
+print("Done you rule Breaker!\n")
+
+#-------------------------------------------------------------------------------
+
+#continue
+for letter in "laxmikant":
+    if letter == "a":
+        continue
+    print(letter)
+
+print("Done you rule maker!")
