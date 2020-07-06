0e04f1d51d9621efdf579201e2b7fa160f0fc59a
Shaunak Chitare <shaunak.chitare@gmail.com>
2020-07-06 20:46:32 +0530

    return statement practice program

---
+++ return_statement_example.py
@@ -0,0 +1,26 @@
+def add(num_list):
+    if len(num_list) == 0:
+        return 0
+
+    r = sum(num_list)
+    return r
+
+#-------------------------------------------------------------------------------
+
+# Break
+for letter in "shaunak":
+    if letter == "a":
+        break
+    print(letter)
+
+print("Done you rule Breaker!\n")
+
+#-------------------------------------------------------------------------------
+
+#continue
+for letter in "laxmikant":
+    if letter == "a":
+        continue
+    print(letter)
+
+print("Done you rule maker!")
